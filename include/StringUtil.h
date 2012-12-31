a3a81554926853a3613fe54fe1e7b2770c6c401f
Daniel Schulte <trilader@gmail.com>
2012-12-31 23:30:31 +0100

    Added string utility functions

---
+++ include/StringUtil.h
@@ -0,0 +1,30 @@
+#ifndef STRING_UTIL_H
+#define STRING_UTIL_H
+
+#include <iostream>
+#include <vector>
+
+namespace StringUtil
+{
+    std::vector<std::string> split(std::string str, const char c='\n')
+    {
+        std::string tmp=str;
+        std::vector<std::string> res;
+        size_t pos=std::string::npos;
+        while((pos=tmp.find_first_of(c))!=std::string::npos)
+        {
+            res.push_back(tmp.substr(0,pos));
+            tmp=tmp.substr(pos+1);
+        }
+        return res;
+    }
+
+    int count(std::string str, const char c)
+    {
+        int res=0;
+        while(str.find(c)!=std::string::npos) ++res;
+        return res;
+    }
+}
+
+#endif // STRING_UTIL_H
