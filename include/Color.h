6028ee35271fff86645f76efbf8a5a2edf09058d
Daniel Schulte <trilader@gmail.com>
2013-01-01 23:23:17 +0100

    Added Color class

---
+++ include/Color.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <SDL/SDL.h>
+
+class Color
+{
+public:
+    Color();
+    Color(Uint8 r, Uint8 g, Uint8 b);
+    Color(Uint32 pix, SDL_PixelFormat *fmt);
+    Uint32 toPixel(SDL_PixelFormat *fmt) const;
+    void fromPixel(Uint32 pix, SDL_PixelFormat *fmt);
+    Uint8 getR() const;
+    Uint8 getG() const;
+    Uint8 getB() const;
+    bool operator==(Color other);
+private:
+    Uint8 r,g,b;
+};
+
+#endif // COLOR_H
---
+++ src/Color.cpp
@@ -0,0 +1,50 @@
+#include "Color.h"
+
+Color::Color()
+{
+    this->r=0;
+    this->g=0;
+    this->b=0;
+}
+
+Color::Color(Uint8 r, Uint8 g, Uint8 b)
+{
+    this->r=r;
+    this->g=g;
+    this->b=b;
+}
+
+Color::Color(Uint32 pix, SDL_PixelFormat *fmt)
+{
+    fromPixel(pix, fmt);
+}
+
+Uint32 Color::toPixel(SDL_PixelFormat *fmt) const
+{
+    return SDL_MapRGB(fmt, r, g, b);
+}
+
+void Color::fromPixel(Uint32 pix, SDL_PixelFormat *fmt)
+{
+    SDL_GetRGB(pix, fmt, &r, &g, &b);
+}
+
+Uint8 Color::getR() const
+{
+    return r;
+}
+
+Uint8 Color::getG() const
+{
+    return g;
+}
+
+Uint8 Color::getB() const
+{
+    return b;
+}
+
+bool Color::operator==(Color other)
+{
+    return r==other.getR()&&g==other.getG()&&b==other.getB();
+}
